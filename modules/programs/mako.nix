7dcfbaccb97da6a10a09ebe78ed10b4a55e30268
Agathangelos Stylianidis <agathangelos.stylianidis@gmail.com>
2023-09-12 17:28:03 +0300

    notification change location

--- modules/programs/mako.nix
+++ modules/programs/mako.nix
@@ -4,7 +4,7 @@
     # iconPath =
     # font = "${config.fontProfiles.regular.family} 12";
     padding = "10,20";
-    anchor = "top-center";
+    anchor = "top-right";
     width = 400;
     height = 150;
     borderSize = 2;
