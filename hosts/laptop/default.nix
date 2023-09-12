230284d5449eb0e19e19b431b3bcbdb222db435e
Agathangelos Stylianidis <agathangelos.stylianidis@gmail.com>
2023-09-12 17:27:30 +0300

    formatting

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -6,16 +6,17 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/display-manager
       ../../modules/desktop-environment
-#       ../../modules/hyprland
+      #       ../../modules/hyprland
       ../../modules/window_manager/sway
       ../../modules/programs/swaylock
     ];
 
-  boot = { 
+  boot = {
     loader = {
       efi = {
         canTouchEfiVariables = true;
@@ -28,7 +29,8 @@
       #   useOSProber = true;
       #   configurationLimit = 20;
       # };
-      systemd-boot = {                    # Use the systemd-boot EFI boot loader.
+      systemd-boot = {
+        # Use the systemd-boot EFI boot loader.
         enable = true;
         configurationLimit = 20;
         consoleMode = "max";
@@ -50,23 +52,44 @@
   networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
 
-#  services.xserver.xkbOptions = {
-#    "eurosign:e";
-#    "caps:escape" # map caps to escape.
-#  };
+  #  services.xserver.xkbOptions = {
+  #    "eurosign:e";
+  #    "caps:escape" # map caps to escape.
+  #  };
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-#  services.xserver.layout = "us,gr";
+  # services.xserver.libinput.enable = true;
+  #  services.xserver.layout = "us,gr";
+  services.xserver = {
+    enable = true;
+    # Configure keymap in X11
+    layout = "us, gr";
+    xkbOptions = "grp:alt_shift_toggle";
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput = {
+      enable = true;
+      # touchpad = {
+      #   tapping = true;
+      #   clickMethod = "clickfinger";
+      # };
+    };
+    # synaptics = {
+    #   enable = true;
+    # };
+    displayManager = {
+      sddm = {
+        enable = true;
+      };
+    };
+  };
 }
-
