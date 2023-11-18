28440c6cd131f97d71a4df2477e1b4c296ec16d4
Agathangelos Stylianidis <agathangelos.stylianidis@gmail.com>
2023-11-18 23:43:00 +0200

    adding pass

--- flake.nix
+++ flake.nix
@@ -1,32 +1,23 @@
 {
-  description = "My Personal NixOS and Darwin System Flake Configuration";
+  description = "My Personal NixOS System Flake Configuration";
 
-  inputs = # All flake references used to build my NixOS setup. These are dependencies.
+  inputs =
     {
-      # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";                  # Nix Packages
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # Nix Packages
-
-      # Latest stable branch of nixpkgs, used for version rollback
-      # The current latest version is 23.05
-      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
-
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
       nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-      nix-colors.url = "github:misterio77/nix-colors";
-      sops-nix.url = "github:mic92/sops-nix";
 
       home-manager = {
-        # User Package Management
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
       };
+
+      nix-colors.url = "github:misterio77/nix-colors";
+      sops-nix.url = "github:mic92/sops-nix";
 
       astro-nvim = {
         url = "github:AstroNvim/AstroNvim";
         flake = false;
       };
-      # nvim-config = {
-      #   url = "github:Mic92/dotfiles";
-      # };
 
       # petclinic = {
       #   # url = "path:/home/angelos/Documents/git/douglas-adams-quotes";
@@ -38,7 +29,6 @@
       #   url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       #   inputs.nixpkgs.follows = "nixpkgs";
       # };
-
     };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
